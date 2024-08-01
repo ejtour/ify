9195353bd7ae53e7a5ea23f0a8c6f29d7ca3e056
edy <bumingyang@ifengyu.com>
2024-08-01 09:24:17 +0800

    add DonutProgressView

---
+++ app/src/main/java/com/ifengyu/aview/utils/UIUtils.java
@@ -0,0 +1,29 @@
+package com.ifengyu.aview.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class UIUtils {
+    /**
+     * sp转px
+     *
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * dp转px
+     *
+     * @param dpValue dp值
+     * @return px值
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+}
